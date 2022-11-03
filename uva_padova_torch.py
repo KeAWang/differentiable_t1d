ed854fd8a054cecf02fd20c0cb974b1225e628a6
Alex Wang <kaw293@cornell.edu>
2022-11-02 17:17:19 -0700

    constrain everything to be non-negative; make sure to integrate with at least dt=1.0 min

--- uva_padova_torch.py
+++ uva_padova_torch.py
@@ -23,6 +23,7 @@ def uva_padova_2008_dynamics(params: Params, t, state: torch.Tensor, carbs: torc
     Returns:
         dstate_dt: state time derivative
     """
+    state = torch.clamp(state, min=0.)  # clamp to avoid negative values
     q_sto1, q_sto2, q_gut, G_p, G_t, I_p, X, I_1, I_d, I_l, I_sc1, I_sc2, Gs = torch.split(state, 1, dim=-1)
 
     #### Oral glucose subsystem
@@ -68,44 +69,37 @@ def uva_padova_2008_dynamics(params: Params, t, state: torch.Tensor, carbs: torc
     Uid = Vmt * G_t / (Kmt + G_t) # nonlinear Michaelis-Menten term for insulin dependent glucose utilization
     # Equation 1 top of Dalla Man et al., 2007b
     dG_p = EGP + Ra - Uii - E - params.k1 * G_p + params.k2 * G_t
-    dG_p = dG_p * (G_p > 0.0)  # Constrain non-negative 
     # Equation 1 middle of Dalla Man et al., 2007b
     dG_t = -Uid + params.k1 * G_p - params.k2 * G_t
-    dG_t = dG_t * (G_t > 0.0) # Constrain non-negative 
     
     #### Insulin kinetics
     # Equation 3 middle of Dalla Man et al., 2007b
     # Extra terms come from subcutaneous insulin (Ra_I): Equation A15 of Dalla Man et al., 2014
     Ra_I = params.m1 * I_l + params.ka1 * I_sc1 + params.ka2 * I_sc2
     dI_p = -(params.m2 + params.m4) * I_p + Ra_I
-    dI_p = dI_p * (I_p > 0.0)# Constrain non-negative
+    # Equation 3 top of Dalla Man et al., 2007b
+    dI_l= -(params.m1 + params.m30) * I_l + params.m2 * I_p
     # Equation 3 bottom of Dalla Man et al., 2007b
     I = I_p / params.Vi
     # Equation 18 of Dalla Man et al., 2007b
-    dX = -params.p2u * X + params.p2u * (I - params.Ib)
+    dX = -params.p2u * X + params.p2u * torch.clamp(I - params.Ib, min=0.)
     # Equation 11 of Dalla Man et al., 2007b
     dI_1= -params.ki * (I_1 - I)
     dI_d= -params.ki * (I_d - I_1)
-    # Equation 3 top of Dalla Man et al., 2007b
-    dI_l= -(params.m1 + params.m30) * I_l + params.m2 * I_p
-    dI_l= dI_l* (I_l > 0.0)# Constrain non-negative
 
     #### subcutaneous insulin kinetics
     # Equation 1 of Dalla Man et al., 2007a
     dI_sc1= insulin - (params.ka1 + params.kd) * I_sc1
-    dI_sc1= dI_sc1* (I_sc1 > 0.0)# Constrain non-negative
 
     dI_sc2= params.kd * I_sc1 - params.ka2 * I_sc2
-    dI_sc2= dI_sc2* (I_sc2 > 0.0)# Constrain non-negative
 
     #### subcutaneous glucose
     # Equation 7 of Dalla Man et al., 2007a 
     # There is a typo in Equation 7, it should be G_p(t) instead of G(t) see https://github.com/jxx123/simglucose/issues/20
     dGs= -params.ksc * Gs + params.ksc * G_p
-    dGs= dGs* (Gs > 0.0)# Constrain non-negative
 
     dstate_dt = torch.cat([dq_sto1, dq_sto2, dq_gut, dG_p, dG_t, dI_p, dX, dI_1, dI_d, dI_l, dI_sc1, dI_sc2, dGs], dim=-1)
-    return dstate_dt
+    return dstate_dt, dict(Ra=Ra, Ra_I=Ra_I, EGP=EGP, E=E, Uid=Uid, Vmt=Vmt, Kmt=Kmt)
 
 def observe_blood_glucose(params:Params, state):
     G = state[..., 3] / params.Vg
